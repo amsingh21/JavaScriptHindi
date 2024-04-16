02ad66f02c3ced8268e12cb22d56d8a0f26bc415
Amita Singh <cs.amitasingh@gmail.com>
2024-04-16 19:46:24 +0000

    data types in JS

--- 01_Basics/02_dataypes.js
+++ 01_Basics/02_dataypes.js
@@ -1,9 +1,23 @@
-"use strict" // treat all  JS code as newer version
+"use strict" ;// treat all  JS code as newer version
 
 // alert("Hello") // we are using node js and not browser so it will throw error
 
-console.log(3+3)
+console.log(3+3)  // code readbility should be high
 console.log("Amita")
 let name ="Amita"
 let age =25
 let isLoggedIn = false
+
+//number
+//bigint
+//string ""
+//boolean true , false 
+//null > standalone value
+//undefined >> value is not defined or assigned 
+
+//symbol unique 
+
+//object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)  //object
