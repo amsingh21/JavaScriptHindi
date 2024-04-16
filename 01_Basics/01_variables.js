dcb7e1677ae9532beb602f31494ce0ce7f3ddc84
Amita Singh <cs.amitasingh@gmail.com>
2024-04-16 17:12:39 +0000

    JS Basics

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 123445
+let emailId= "cs.amitasingh@gmail.com"
+var accountPassword="1234"
+accountCity="Dallas"
+accountStatus
+/* 
+  please Prefer not use var due to block scope and functionaly scope 
+*/
+// accountId=234 // not allowed to change value of contants 
+accountCity="Plano"
+emailId="cs.amsingh@gmail.com"
+accountPassword="23498"
+//console.log(accountId);
+console.table([accountId,accountCity,emailId,accountPassword, accountStatus]);
---
+++ 01_Basics/02_dataypes.js
@@ -0,0 +1,9 @@
+"use strict" // treat all  JS code as newer version
+
+// alert("Hello") // we are using node js and not browser so it will throw error
+
+console.log(3+3)
+console.log("Amita")
+let name ="Amita"
+let age =25
+let isLoggedIn = false
