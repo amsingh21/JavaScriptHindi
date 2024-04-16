4c32c6b41c83fb3327f1dea893cd8e9e1603e09e
Amita Singh <cs.amitasingh@gmail.com>
2024-04-16 21:36:57 +0000

    conversion operations

--- 01_Basics/03_conversionoperation.js
+++ 01_Basics/03_conversionoperation.js
@@ -25,5 +25,34 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let rollNumber= "33"
 let stringRollNumber = String(rollNumber)
 
-console.log(stringRollNumber);
-console.log(typeof(stringRollNumber));
+//console.log(stringRollNumber);
+//console.log(typeof(stringRollNumber));
+
+//*****************************  Operations  ********************************* */
+
+let value = 3;
+let negvalue = -value
+
+//console.log(negvalue);
+
+let str1= "Hello"
+let str2 = "  Amita"
+// console.log(str1+ str2);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2 , num3 
+
+num1 = num2 = num3 = 2+2 
+
+let gameCounter= 100
+
+//gameCounter++
+++gameCounter
+
+console.log(gameCounter);
