35f9b4c18c32e4a72dd51400e4c68b6a6b0c2586
Amita Singh <cs.amitasingh@gmail.com>
2024-04-16 20:06:18 +0000

    conversion of data types

---
+++ 01_Basics/03_conversionoperation.js
@@ -0,0 +1,29 @@
+//let score = "33abc"
+//let score= null;
+//let score = undefined
+//let score = false
+let score = "Amita"
+//console.log(typeof score)
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber)
+
+//33 => 33
+// "33abc" => NaN
+// false => 0 ; true => 1
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=> true 0=> false
+// empty string => flase , string value => true
+
+let rollNumber= "33"
+let stringRollNumber = String(rollNumber)
+
+console.log(stringRollNumber);
+console.log(typeof(stringRollNumber));
