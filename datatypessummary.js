714b58d65d510ac6a5362dd3e3a0fbd94d24d7fa
Amita Singh <cs.amitasingh@gmail.com>
2024-04-16 23:49:16 +0000

    data types

--- datatypessummary.js
+++ datatypessummary.js
@@ -26,7 +26,7 @@ const bigNumber = 34567788987
 //console.log(bigNumber);
 
 //console.log(typeof(bigNumber));
-console.log(typeof(outsideTemp)); // returns data type as object
+//console.log(typeof(outsideTemp)); // returns data type as object
 
 // Reference type or non Primitive type : Arrays , Objects , Functions 
 
@@ -49,3 +49,30 @@ let myObj = {
  //console.log(typeof(myFunction)); // object function data type of function 
 
  //console.log(typeof(myObj)); // Non primitive data types always return data type as Object 
+
+
+ //++++++++++++++++++++++++++++++++++++++  Stack Memory  and Heap Memory +++++++++++++++++++++++++++++++++++++++++++
+
+ // Stack memory  => Primitive is used in primitive data type  here copy is created 
+ // Heap memory => Non Primitive  is used  by non primitive data type  here reference is created 
+
+ let myYoutubeName = "codewithMily"
+
+ let anotherName = myYoutubeName
+
+   anotherName = "codewithAmita"
+
+//  console.log(anotherName);
+//  console.log(myYoutubeName);
+
+ let userOne=  {
+    email : "userone@gmail.com",
+    upi: "usa123@ybl.com"
+ }
+
+ let userTwo = userOne
+
+ userTwo.email = "amita@google.com"
+
+ console.log(userTwo.email);
+ console.log(userOne.email);
