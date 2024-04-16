691f58c6c11e92b6eb3a1eafd1e84a8065d40121
Amita Singh <cs.amitasingh@gmail.com>
2024-04-16 23:48:54 +0000

    Strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,30 @@
+const name= "Amita"
+
+const repoCount = 50 
+
+//console.log(name + repoCount + " Value"); // this is not readable approach so avoid writing code like this 
+
+//console.log(`Hello my name is ${name} and my total repo coiunt is ${repoCount}`);
+
+const gameName = new String("PlaywithMily") // another way to use string data type 
+
+// console.log(gameName[0]);
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+
+// console.log(gameName.indexOf('a'));
+
+ const newString = gameName.substring(0,4)
+
+
+ console.log(newString);
+
+ const anotherString = gameName.slice(-4,0)
+
+ console.log(anotherString)
