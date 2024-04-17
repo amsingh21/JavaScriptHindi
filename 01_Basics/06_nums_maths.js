f8fa07a23c1028683058788f241a86cae09daa99
Amita Singh <cs.amitasingh@gmail.com>
2024-04-17 18:45:00 +0000

    Math function

---
+++ 01_Basics/06_nums_maths.js
@@ -0,0 +1,61 @@
+const score = 400
+
+const balance = new Number(600)
+
+const newNumber = 138.898
+
+// console.log(balance);
+
+// console.log(score);
+
+
+// console.log(balance.toString().length);
+
+
+
+//console.log(balance.toFixed(2));
+
+
+// console.log(newNumber.toPrecision(4));
+
+const hundred = 10000000
+
+// console.log(hundred.toLocaleString());
+
+// console.log(hundred.toLocaleString('en-IN'));
+
+//------------------------------MATHS----------------------------
+
+// console.log(Math)
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(5.2));
+
+// console.log(Math.ceil(5.1));
+
+// console.log(Math.floor(4.2));
+
+// console.log(Math.sqrt(4));
+
+// console.log(Math.min(4,8,2,5,7));
+
+// console.log(Math.max(4,2,6,9,11,3));
+
+// console.log(Math.random());
+
+// console.log((Math.random()*10) +1);
+
+// console.log(Math.floor(Math.random()* 10) + 1);
+
+
+const min = 10
+
+const max = 20
+
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+// random method gives values between 0 to 1 and to avoid 0 values then add 1 and multiply by 10 and take floor and add value as min to get values between range 
+
+
