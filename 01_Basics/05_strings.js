ca2a0204209bbb545d983dbf518ed033e57908af
Amita Singh <cs.amitasingh@gmail.com>
2024-04-17 17:48:13 +0000

    string methods

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -22,15 +22,35 @@ const gameName = new String("PlaywithMily") // another way to use string data ty
 
  //const newString = gameName.substring(0,4)
 
+// console.log(gameName.length);
 
- //console.log(newString);
-//  const str = "The morning is upon us.";
-const str3=  "BMW, Audi, Mercedes";
-console.log(str3.slice(-12,-6));
+// const str1= gameName.slice(-6,2)
+// console.log(str1);
+//  //console.log(newString);
 
- const anotherString = gameName.slice(-4,10)
+const str1 = "The morning is upon us."; // The length of str1 is 23.
+const str2 = str1.slice(1, 8);
+const str3 = str1.slice(4, -6);
+const str4 = str1.slice(12);
+const str5 = str1.slice(30);
+// console.log(str2); // he morn
+// console.log(str3); // morning is upon u
+// console.log(str4); // is upon us.
+// console.log(str5); // ""
 
 
+const newString1 = new String("  AMita   ")
+
+console.log(newString1.trim());
+
+
+const url = "https://amitasingh.com/amita%20page"
+
+console.log( url.replace("%20","-"))
+
+console.log(url.includes("amita"));
+
+console.log(url.split("%"));
 
  //console.log(anotherString)
 // console.log(str);
