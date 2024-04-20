d5729717de804cf373931098ce750b3d74aa7895
Amita Singh <cs.amitasingh@gmail.com>
2024-04-20 21:20:40 +0000

    truthy

--- truthy.js
+++ truthy.js
@@ -1,10 +1,10 @@
 const userEmail= []
 
 if (userEmail){
-    console.log("found user email");
+    //console.log("found user email");
 }
 else {
-    console.log("User Email does not exists");
+    //console.log("User Email does not exists");
 }
 
 // falsy values 
@@ -18,11 +18,30 @@ else {
 // "0" , 'false', " ", [] , {} , function(){}
 
 if (userEmail.length === 0 ){
-    console.log("Array is empty ");
+   // console.log("Array is empty ");
 }
 
 const emptyObj = {}
 
 if (Object.keys(emptyObj).length == 0 ){
-    console.log("Object is empty");
+   // console.log("Object is empty");
 }
+
+//Nullish Coalescing operator (?? ) : null , undefined 
+
+let val1 ;
+val1 = 5 ?? 10 
+val1 = null ?? 10
+//val1 = undefined ?? 15
+val1 = null ?? 10 ?? 15
+
+//console.log(val1);
+
+// Terniary operator 
+
+//condition ? true : false 
+
+
+const iceTeaPrice = 100
+
+iceTeaPrice >= 80 ? console.log("price is less than 100") : console.log("Price is greater than 100");;
