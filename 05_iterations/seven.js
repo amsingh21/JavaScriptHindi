6f897acad69cf34b039a67e11d5f5a70077070a8
Amita Singh <cs.amitasingh@gmail.com>
2024-04-22 23:21:33 +0000

    maps filter

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -2,8 +2,16 @@ const myNumbers = [1,2,3,4,5,6,7,8,9,10]
 
 //const newNums = myNumbers.map((nums) =>  { return nums +10  })
 
-const newNums =  myNumbers.forEach( (num ) => { 
-    console.log(num +10);  
- })
+// const newNums =  myNumbers.forEach( (num ) => { 
+//     console.log(num +10);  
+//  })
 
+const newNums = myNumbers
+.map(( num) => num*10 )
+.map(( num) => num+1)
+.filter( (num)=> (num >40) )
+    
+
+console.log(newNums);
 //console.log(newNums);
+
