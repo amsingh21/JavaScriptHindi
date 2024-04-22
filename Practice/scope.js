5c1c1d5f94a687a5d5f04eedae357d4ab523b690
Amita Singh <cs.amitasingh@gmail.com>
2024-04-22 15:22:54 +0000

    practice

---
+++ Practice/scope.js
@@ -0,0 +1,25 @@
+function one (){
+    const username = "Amita"
+    function two(){
+        const website = "Youtube"
+
+        //console.log(username);
+    }
+    //console.log(website);
+     two()
+}
+
+one()
+
+if(true){
+    const username = "Amita"
+    if (username === "Amita"){
+        const website = " Youtube"
+
+        console.log(username + website);
+    }
+
+    //console.log(website);
+}
+
+//console.log(username);
