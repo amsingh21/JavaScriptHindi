bab0b7a8ebfb5cea54f33a980f011e8d7d953814
Amita Singh <cs.amitasingh@gmail.com>
2024-04-22 23:06:41 +0000

    map for each

---
+++ 05_iterations/seven.js
@@ -0,0 +1,9 @@
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+//const newNums = myNumbers.map((nums) =>  { return nums +10  })
+
+const newNums =  myNumbers.forEach( (num ) => { 
+    console.log(num +10);  
+ })
+
+//console.log(newNums);
