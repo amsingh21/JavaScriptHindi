b634a1fd4acb18631ff87b18aced099f5cdbcbed
Amita Singh <cs.amitasingh@gmail.com>
2024-04-22 19:49:10 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let index = 0
+while (index <=10){
+   // console.log((`value of index is ${index}`));
+    index = index +2 
+}
+
+let myArray = ['Flash', 'Batman','Superman']
+let arr =0
+while(arr < myArray.length ){
+   // console.log(`DC Heros : ${myArray[arr]}`);
+    arr ++
+}
+
+let score = 11
+do {
+    console.log(`Score value is ${score}`);
+
+    score++
+    
+} while (score <= 10);
